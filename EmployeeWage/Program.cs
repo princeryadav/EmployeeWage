21477831df744f27f5181a11e3456aaa4d1a7db3
princeryadav <prince.yadavp098@gmail.com>
2020-09-28 15:29:49 +0530

    [UC1] Refactor: mdifying main method

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,6 +7,8 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            EmployeePresOrAbs employeePresOrAbs = new EmployeePresOrAbs();
+            employeePresOrAbs.IsEmployeePresentOrAbsent();
         }
     }
 }
