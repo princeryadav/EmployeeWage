d792dceced20b6be4b3e40907566e3ea7fe82295
princeryadav <prince.yadavp098@gmail.com>
2020-09-28 15:27:05 +0530

    [UC1] Add: Adding new Branch and New file

---
+++ EmployeeWage/EmployeePresOrAbs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    class EmployeePresOrAbs
+    {
+        readonly int IS_FULL_TIME;
+        public EmployeePresOrAbs()
+        {
+            this.IS_FULL_TIME = 1;
+        }
+        public void IsEmployeePresentOrAbsent()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == this.IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplpyee is Absent");
+            }
+        }
+    }
+}
