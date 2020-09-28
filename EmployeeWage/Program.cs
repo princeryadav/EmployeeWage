b05846a7f679b45edd3009b9dabaded84dd239c8
princeryadav <prince.yadavp098@gmail.com>
2020-09-28 16:57:55 +0530

    [UC4] Refactor: Modifying main method

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,15 +6,21 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
+            // Master Branch
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            // UC1
             EmployeePresOrAbs employeePresOrAbs = new EmployeePresOrAbs();
             employeePresOrAbs.IsEmployeePresentOrAbsent();
+            // UC2
             EmployeeWageCal wageCal = new EmployeeWageCal();
             wageCal.CalculateEmpWage();
+            // UC3
             EmployeePartTime partTime = new EmployeePartTime();
             partTime.AddPartTimeEmployee();
+            // UC4
             EmployeePartTimeSwitch partTimeSwitch = new EmployeePartTimeSwitch();
             partTimeSwitch.AddPartTimeEmployee();
+            // UC5
         }
     }
 }
