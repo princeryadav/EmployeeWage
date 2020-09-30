627090e5f1ad02c6647c1352f5bc8dc4646c6468
princeryadav <prince.yadavp098@gmail.com>
2020-09-30 18:59:02 +0530

    [UC9] Add: adding new Branch

---
+++ EmployeeWageOOPs/EmpWageBuilderObj.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageOOPs
+{
+    class EmpWageBuilderObj
+    {
+        // All constant.
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDay;
+        private int maxHourPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObj(string company, int empRatePerHour, int numOfWorkingDay, int maxHourPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDay = numOfWorkingDay;
+            this.maxHourPerMonth = maxHourPerMonth;
+        }
+
+        public void ComputeEmpWage()
+        {
+            // All Local.
+            int empHrs;
+            int totalNumOfHrs = 0;
+            int totalNumOfDay = 0;
+            // Check for condition.
+            while (totalNumOfHrs <= this.maxHourPerMonth && totalNumOfDay < this.numOfWorkingDay)
+            {
+                totalNumOfDay++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalNumOfHrs += empHrs;
+            }
+            totalEmpWage = totalNumOfHrs * this.empRatePerHour;
+            Console.WriteLine("Total num of Wage for company : {0} is {1}", company, totalEmpWage);
+        }
+
+        public string toString()
+        {
+            return "Total num of Wage for company : " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWageOOPs/Program.cs
+++ EmployeeWageOOPs/Program.cs
@@ -4,46 +4,16 @@ namespace EmployeeWageOOPs
 {
     class Program
     {
-        // All constant.
-        private const int IS_PART_TIME = 1;
-        private const int IS_FULL_TIME = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Emp computation!");
-            ComputeEmpWage("DMART", 20, 20, 100);
-            ComputeEmpWage("JIO De dana dan", 20, 20, 100);
-        }
-
-        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDay, int maxHourPerMonth)
-        {
-            // All Local.
-            int empHrs;
-            int totalNumOfWage = 0;
-            int totalNumOfHrs = 0;
-            int totalNumOfDay = 0;
-            // Check for condition.
-            while (totalNumOfHrs <= maxHourPerMonth && totalNumOfDay < numOfWorkingDay)
-            {
-                totalNumOfDay++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalNumOfHrs += empHrs;
-            }
-            totalNumOfWage = totalNumOfHrs * empRatePerHour;
-            Console.WriteLine("Total num of Wage for company : {0} is {1}", company, totalNumOfWage);
-            return totalNumOfWage;
+            EmpWageBuilderObj builderObj = new EmpWageBuilderObj("DMART", 20,20,100);
+            builderObj.ComputeEmpWage();
+            string result =builderObj.toString();
+            Console.WriteLine(result);
+            EmpWageBuilderObj builderObj1 = new EmpWageBuilderObj("TATA", 20, 20, 100);
+            builderObj1.ComputeEmpWage();
+            Console.WriteLine(builderObj1.toString());
         }
     }
 }
