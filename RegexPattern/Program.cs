57a1110d99f030d3aeeba66af4876a053d0717cc
princeryadav <prince.yadavp098@gmail.com>
2020-10-06 09:22:01 +0530

    [regex-pattern] Add: adding new project

---
+++ RegexPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex Pattern Demo!");
+        }
+    }
+}
